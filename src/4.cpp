d3225dcb2bbe89a2ad3cc31950fa2e6b05546ba6
sharieg3ebw <sharieg3ebw@gmx.com>
2025-03-07 13:35:05 +0000

    Add 4.cpp

---
+++ src/4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x = 0; // set x to zero
+    while (x <= 10) { // while loop condition
+        cout << "x is equal to: " << x << endl; // print x value
+        x += 0.25; // add 0.25 to x
+    }
+    return 0;
+}
